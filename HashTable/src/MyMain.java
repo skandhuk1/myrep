80599412e303c0dc8214aed3a19a2da581f39cbc
Srinivas Kandhukoori <61404356+skandhuk1@users.noreply.github.com>
2022-09-10 17:48:51 +0530

    HashTable

---
+++ HashTable/src/MyMain.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		   Employee janeJones = new Employee("Jane", "Jones", 123);
+	        Employee johnDoe = new Employee("John", "Doe", 4567);
+	        Employee marySmith = new Employee("Mary", "Smith", 22);
+	        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+	        Employee billEnd = new Employee("Bill", "End", 78);
+	        
+	        Map<String, Employee> hashMap = new HashMap<String, Employee>();
+	        hashMap.put("Jones", janeJones);
+	        hashMap.put("Doe", johnDoe);
+	        hashMap.put("Smith", marySmith);
+	        hashMap.put("Wilson", mikeWilson);
+	        
+//	        Iterator<Employee> iterator = hashMap.values().iterator();
+//	        while(iterator.hasNext()) {
+//	        	System.out.println(iterator.next());
+//	        }
+	        
+	        hashMap.forEach((k, v) -> System.out.println("Key = " + k + ", Employee = " + v));
+	        
+
+	}
+
+}
